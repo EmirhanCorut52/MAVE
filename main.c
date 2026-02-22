0f854bc7ec7ad1807c79b460d08193ab16fbf4a3
Emirhan wsl <ecorut4@gmail.com>
2026-02-22 13:44:56 +0300

    main.c modified

--- main.c
+++ main.c
@@ -8,7 +8,16 @@
 #define HEIGHT 1920
 #define THRESHOLD 5
 
-long calculate_frame_difference (uint8_t *frame1, uint8_t *frame2, int size) {
+static int write_nals(FILE *file_out, x264_nal_t *nals, int i_nals) {
+    for (int i = 0; i < i_nals; i++) {
+        if (fwrite(nals[i].p_payload, 1, nals[i].i_payload, file_out) != (size_t)nals[i].i_payload) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+long calculate_frame_difference(uint8_t *frame1, uint8_t *frame2, int size) {
     long total_diff = 0;
 
     if (size == 0) return 0;
@@ -21,17 +30,39 @@ long calculate_frame_difference (uint8_t *frame1, uint8_t *frame2, int size) {
     return total_diff / size;
 }
 
-int main() {
-    FILE *file_in = fopen("yuv/orta.yuv", "rb");
-    FILE *file_out = fopen("h264/orta.h264", "wb");
-    FILE *file_tc = fopen("time/orta.txt", "w");
+int main(int argc, char *argv[]) {
 
-    fprintf(file_tc, "# timecode format v2\n");
-
-    if (!file_in || !file_out) {
-        printf("Dosyalar Acilamadi!");
+    if (argc < 2) {
+        printf("Dosya ismi girilmedi");
         return -1;
     }
+
+    const char *base_name = argv[1];
+    char yuv_path[256], h264_path[256], tc_path[256], aac_path[256], mkv_path[256], mov_path[256];
+    char cmd_aac[2048], cmd_mkv[2048], cmd_yuv[2048];
+
+    snprintf(yuv_path, sizeof(yuv_path), "yuv/%s.yuv", base_name);
+    snprintf(h264_path, sizeof(h264_path), "h264/%s.h264", base_name);
+    snprintf(tc_path, sizeof(tc_path), "time/%s.txt", base_name);
+    snprintf(aac_path, sizeof(aac_path), "aac/%s.aac", base_name);
+    snprintf(mkv_path, sizeof(mkv_path), "mkv/%s.mkv", base_name);
+    snprintf(mov_path, sizeof(mov_path), "mov/%s.mov", base_name);
+    snprintf(cmd_yuv, sizeof(cmd_yuv), "ffmpeg -y -i '%s' -c:v rawvideo -pix_fmt yuv420p -s %dx%d '%s' -loglevel warning", mov_path, WIDTH, HEIGHT, yuv_path);
+    system(cmd_yuv);
+
+    FILE *file_in = fopen(yuv_path, "rb");
+    FILE *file_out = fopen(h264_path, "wb");
+    FILE *file_tc = fopen(tc_path, "w");
+
+    if (!file_in || !file_out || !file_tc) {
+        printf("Dosyalar Acilamadi!");
+        if (file_in) fclose(file_in);
+        if (file_out) fclose(file_out);
+        if (file_tc) fclose(file_tc);
+        return -1;
+    }
+
+    fprintf(file_tc, "# timecode format v2\n");
 
     x264_param_t param;
     x264_param_default_preset(&param, "medium", "zerolatency");
@@ -43,6 +74,7 @@ int main() {
     param.i_timebase_num = 1;
     param.i_timebase_den = 30;
     param.b_vfr_input = 1;
+    param.i_bframe = 0;
 
     x264_t *encoder = x264_encoder_open(&param);
 
@@ -52,19 +84,34 @@ int main() {
     }
 
     x264_picture_t pic_in, pic_out;
-    x264_picture_alloc(&pic_in, X264_CSP_I420, WIDTH, HEIGHT);
+    if (x264_picture_alloc(&pic_in, X264_CSP_I420, WIDTH, HEIGHT) != 0) {
+        printf("x264_picture_alloc basarisiz!\n");
+        x264_encoder_close(encoder);
+        fclose(file_in);
+        fclose(file_out);
+        fclose(file_tc);
+        return -1;
+    }
 
     int y_size = WIDTH * HEIGHT;
-    int frame_size = y_size * 3 / 2;
 
-    uint8_t *prev_frame_y = (uint8_t*)calloc(frame_size, 1);
+    uint8_t *prev_frame_y = (uint8_t*)calloc(y_size, 1);
+    if (!prev_frame_y) {
+        printf("Bellek ayrilamadi!\n");
+        x264_picture_clean(&pic_in);
+        x264_encoder_close(encoder);
+        fclose(file_in);
+        fclose(file_out);
+        fclose(file_tc);
+        return -1;
+    }
 
     int frame_count = 0;
     int encoded_count = 0;
 
     while (fread(pic_in.img.plane[0], 1, y_size, file_in) == y_size) {
-        fread(pic_in.img.plane[1], 1, y_size / 4, file_in);
-        fread(pic_in.img.plane[2], 1, y_size / 4, file_in);
+        if (fread(pic_in.img.plane[1], 1, y_size / 4, file_in) != (size_t)(y_size / 4)) break;
+        if (fread(pic_in.img.plane[2], 1, y_size / 4, file_in) != (size_t)(y_size / 4)) break;
 
         frame_count ++;
 
@@ -85,15 +132,22 @@ int main() {
             int frame_bytes = x264_encoder_encode(encoder, &nals, &i_nals, &pic_in, &pic_out);
 
             if (frame_bytes > 0) {
-                fwrite(nals[0].p_payload, 1, frame_bytes, file_out);
+                if (write_nals(file_out, nals, i_nals) != 0) {
+                    printf("H264 yazma hatasi!\n");
+                    break;
+                }
                 encoded_count++;
 
                 long long ms = (long long)(pic_out.i_pts * 1000.0 / 30.0);
                 fprintf(file_tc, "%lld\n", ms);
             }
-            
-            memcpy (prev_frame_y, pic_in.img.plane[0], y_size);
+            else if (frame_bytes < 0) {
+                printf("x264 encode hatasi!\n");
+                break;
+            }
         }
+
+        memcpy(prev_frame_y, pic_in.img.plane[0], y_size);
     }
 
     while (1) {
@@ -103,8 +157,11 @@ int main() {
         
         if (frame_bytes <= 0)
             break;
-            
-        fwrite(nals[0].p_payload, 1, frame_bytes, file_out);
+
+        if (write_nals(file_out, nals, i_nals) != 0) {
+            printf("H264 yazma hatasi!\n");
+            break;
+        }
         encoded_count++;
 
         long long ms = (long long)(pic_out.i_pts * 1000.0 / 30.0);
@@ -113,7 +170,12 @@ int main() {
 
     printf("Toplam Kare: %d\n", frame_count);
     printf("Encode olan kare: %d\n", encoded_count);
-    printf("Tasarruf Orani: %% %.2f\n", (float)encoded_count / frame_count * 100.0);
+    if (frame_count > 0) {
+        float saved_percent = (1.0f - ((float)encoded_count / (float)frame_count)) * 100.0f;
+        printf("Tasarruf Orani: %% %.2f\n", saved_percent);
+    } else {
+        printf("Tasarruf Orani: %% 0.00\n");
+    }
 
     x264_encoder_close(encoder);
     x264_picture_clean(&pic_in);
@@ -122,8 +184,10 @@ int main() {
     fclose(file_out);
     fclose(file_tc);
 
-    system("ffmpeg -y -i mov/orta.mov -vn -c:a aac aac/orta.aac");
-    system("mkvmerge -o mkv/orta.mkv --timestamps 0:time/orta.txt h264/orta.h264 aac/orta.aac");
+    snprintf(cmd_aac, sizeof(cmd_aac), "ffmpeg -y -i '%s' -vn -c:a aac '%s' -loglevel warning", mov_path, aac_path);
+    system(cmd_aac);
+    snprintf(cmd_mkv, sizeof(cmd_mkv), "mkvmerge -o '%s' --timestamps 0:'%s' '%s' '%s'", mkv_path, tc_path, h264_path, aac_path);
+    system(cmd_mkv);
 
     return 0;
 }
